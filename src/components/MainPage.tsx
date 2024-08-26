82ae9d1e937d464b5c4146d3bc0f3842ac225356
EmceeDiamond <vladislav.gontaruk@yandex.ru>
2024-08-26 13:33:55 +0300

    fixed

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,7 +3,7 @@ import { FC, useState } from "react";
 import { Users } from "../types";
 
 const MainPage: FC<{users: Users[]}> = ({users}) => {
-    const [dataStatus, setDataStatus] = useState(true);
+    const [dataStatus, setDataStatus] = useState(false);
 
     return(
         <div>
